96e1f4b66d4889de431e66e0c816ff1fe32f072d
donnasalinasan <donna.salinasan@evsu.edu.ph>
2021-03-21 23:08:10 +0800

    Commit all Files

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  contName="";
+  contNumber="";
+  contacts=[];
+  constructor(public alertController: AlertController)  {}
 
-  constructor() {}
+  saveContact() {
+    let contact = {
+      name: this.contName,
+      number: this.contNumber
+
+    }
+    this.contacts.push(contact);
+    console.log(this.contacts);
+    this.clearField();
+
+  }
+  clearField() {
+    this.contName = "";
+    this.contNumber = "";
+  }
+  
+
+  async deleteConfirm(i) {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Are you sure you want to delete this contact?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+          }
+        }, {
+          text: 'Delete',
+          handler: () => {
+            this.contacts.splice(i,1);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
+    
 
 }
